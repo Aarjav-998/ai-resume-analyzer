895fa5ed0635c7aed0627f049770de1581d38810
Aarjav-998 <web.sidehustle2025@gmail.com>
2025-07-31 10:07:03 +0530

    Update puter.ts 1

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "claude-3-sonnet" }
+      { model: 'mistral-large-latest' }
     ) as Promise<AIResponse | undefined>;
   };
 
